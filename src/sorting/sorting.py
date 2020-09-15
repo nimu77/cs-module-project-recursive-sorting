00921a88c81eacc96e53a480be29f7b0fbf6a12e
nimu77 <nirmal.thapa77@outlook.com>
2020-09-15 15:58:37 -0700

    Nirmal_week3_day2

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,16 +1,39 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+# two arrays that are each already sorted
+# this function returns a single sorted array of all the elements from
+# arrA and arrB
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
+    # pre-allocating our output array with the number of elements
+    # we know it will hold at the end
     merged_arr = [0] * elements
 
     # Your code here
+    start_a = 0
+    start_b = 0
+    # init pointers to the beginning of arrA and arrB
+    # compare the elements those pointers are pointing at
+    # whichever element is less than or equal
+    # loop so long as both
+    #  pointers are in range of their
+    # respective arrays
 
+    output = []
+    for i in range(0, len(merged_arr)):
+        if arrA[start_a] > arrB[start_b]:
+            output.append(arrB[start_b])
+            start_b += 1
+        elif arrA[start_a] < arrB[start_b]:
+            output.append(arrA[start_a])
+            start_a += 1
 
-    return merged_arr
+    return output
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+
+
 
 
     return arr
